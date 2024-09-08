ac9487c368e52ac6fb67cd6fe8c188009f860a1e
Joon <nhocjoon123@gmail.com>
2024-09-08 16:01:48 +0700

    Update

--- BTGKCTDL/BTGKCTDL/BT4.cpp
+++ BTGKCTDL/BTGKCTDL/BT4.cpp
@@ -1,27 +1,58 @@
 #include <iostream>
 using namespace std;
+#define MAX 100
 
 struct Node {
 	int data;
 	Node* left, * right;
 };
 
-Node* createNode(int x){
+Node* createNode(int x) {
 	Node* p = new Node;
 	p->data = x;
 	p->left = p->right = NULL;
 	return p;
 }
 
+struct Stack {
+	Node* a[MAX];
+	int top;
+};
+
+void initStack(Stack& s) {
+	s.top = -1;
+}
+
+void push(Stack& s, Node* x) {
+	if (s.top < MAX)
+		s.a[++s.top] = x;
+}
+
+bool isStackEmpty(Stack s) {
+	return s.top == -1;
+}
+
+Node* top(Stack s) {
+	if (!isStackEmpty(s))
+		return s.a[s.top];
+	return NULL;
+}
+
+Node* pop(Stack& s) {
+	if (!isStackEmpty(s))
+		return s.a[s.top--];
+	return NULL;
+}
+
 void initBinaryTree(Node*& root) {
 	root = NULL;
 }
 
-void insert(Node *&root, int x) {
+void insert(Node*& root, int x) {
 	if (root == NULL)
 		root = createNode(x);
 	else {
-		Node* p = root, *pre = root;
+		Node* p = root, * pre = root;
 		while (p != NULL) {
 			pre = p;
 			if (p->data == x)
@@ -39,10 +70,21 @@ void insert(Node *&root, int x) {
 	}
 }
 
+void recursiveInsert(Node*& root, int x) {
+	if (root == NULL)
+		root = createNode(x);
+	else if (root->data == x)
+		return;
+	else if (root->data > x)
+		recursiveInsert(root->left, x);
+	else if (root->data < x)
+		recursiveInsert(root->right, x);
+}
+
 void del(Node*& root, int key) {
 	if (root != NULL) {
 		Node* p = root, * pre = root;
-		while (p != NULL && p->data!=key) {
+		while (p != NULL && p->data != key) {
 			pre = p;
 			if (p->data > key)
 				p = p->left;
@@ -50,21 +92,95 @@ void del(Node*& root, int key) {
 				p = p->right;
 		}
 		if (p != NULL) {
-			if (p->left != NULL && p->right != NULL) {
+			if (p->left == NULL || p->right == NULL) {
+				if (p == root) {
+					if (p->left != NULL)
+						root = p->left;
+					else if (p->right != NULL)
+						root = p->right;
+					else
+						root = NULL;
+				}
+				else {
+					if (pre->data > key)
+						pre->left = p->left;
+					else
+						pre->right = p->right;
+
+				}
+				delete p;
 
 			}
-			else if (p->left != NULL) {
-				pre
-			}
-			else if (p->right != NULL) {
-
+			else {
+				Node* q = p->right, * preq = p->right;
+				while (q->left != NULL) {
+					preq = q;
+					q = q->left;
+				}
+				p->data = q->data;
+				if (q == preq) {
+					p->right = q->right;
+				}
+				else {
+					preq->left = q->right;
+				}
+				delete q;
 			}
 		}
 	}
 }
 
+
+
+void searchStandForNoRecursion(Node*& p, Node*& q) {
+	Node* pre = NULL;
+	Node* temp = q;
+	while (temp->left != NULL) {
+		pre = temp;
+		temp = temp->left;
+	}
+	p->data = temp->data;
+	p = temp;
+	if (pre == NULL)
+		q = q->right;
+	else
+		pre->left = temp->right;
+}
+
+void searchStandFor(Node*& p, Node*& q) {
+	if (q->left == NULL) {
+		p->data = q->data;
+		p = q;
+		q = q->right;
+	}
+	else {
+		searchStandFor(p, q->left);
+	}
+}
+
+void recursiveDel(Node*& root, int key) {
+	if (root == NULL)
+		return;
+	else if (root->data == key) {
+		Node *p = root;
+		if (root->left == NULL) {
+			root = root->right;
+		}
+		else if (root->right == NULL)
+			root = root->left;
+		else {
+			searchStandFor(p, root->right);
+		}
+		delete p;
+	}
+	else if (root->data > key)
+		recursiveDel(root->left, key);
+	else if (root->data < key)
+		recursiveDel(root->right, key);
+}
+
 bool search(Node* root, int key) {
-	if (root != NULL){
+	if (root != NULL) {
 		Node* p = root;
 		while (p != NULL) {
 			if (p->data == key)
@@ -78,7 +194,7 @@ bool search(Node* root, int key) {
 	return false;
 }
 
-void LNR(Node *root) {
+void LNR(Node* root) {
 	if (root != NULL) {
 		LNR(root->left);
 		cout << root->data << " ";
@@ -86,15 +202,212 @@ void LNR(Node *root) {
 	}
 }
 
+void LRN(Node* root) {
+	if (root != NULL) {
+		LRN(root->left);
+		LRN(root->right);
+		cout << root->data << " ";
+	}
+}
+
+void NLR(Node* root) {
+	if (root != NULL) {
+		cout << root->data << " ";
+		NLR(root->left);
+		NLR(root->right);
+	}
+}
+
+bool isPrimeNumber(int x) {
+	if (x < 2)
+		return false;
+	for (int i = 2; i <= sqrt(x); i++) {
+		if (x % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int countNode(Node* root) {
+	if (root == NULL)
+		return 0;
+	return 1 + countNode(root->left) + countNode(root->right);
+
+}
+
+void countNodeTailRecursion(Node* root, int& count) {
+	if (root == NULL)
+		return;
+	count++;
+	countNodeTailRecursion(root->left,count);
+	countNodeTailRecursion(root->right,count);
+}
+
+int countPrimeNode(Node* root) {
+	if (root == NULL)
+		return 0;
+	if (isPrimeNumber(root->data))
+		return 1 + countPrimeNode(root->left) + countPrimeNode(root->right);
+	else
+		return countPrimeNode(root->left) + countPrimeNode(root->right);
+}
+
+void countPrimeNodeTailRecursion(Node* root, int &count) {
+	if (root == NULL)
+		return;
+	if (isPrimeNumber(root->data))
+		count++;
+	countPrimeNodeTailRecursion(root->left, count);
+	countPrimeNodeTailRecursion(root->right, count);
+}
+
+void LNRNoRecursion(Node* root) {
+	if (root != NULL) {
+		Node* p = root;
+		Stack s;
+		initStack(s);
+		while (!isStackEmpty(s) || p != NULL) {
+			while (p != NULL) {
+				push(s, p);
+				p = p->left;
+			}
+			p = pop(s);
+			cout << p->data << " ";
+			p = p->right;
+		}
+	}
+}
+
+void NLRNoRecursion(Node* root) {
+	if (root != NULL) {
+		Node* p = root;
+		Stack s;
+		initStack(s);
+		while (!isStackEmpty(s) || p != NULL) {
+			while (p != NULL) {
+				cout << p->data << " ";
+				push(s, p);
+				p = p->left;
+			}
+			p = pop(s);
+			p = p->right;
+		}
+	}
+}
+
+void LRNNoRecursion(Node* root) {
+	if (root != NULL) {
+		Node* p = root, * lastVisited = NULL;
+		Stack s;
+		initStack(s);
+		while (!isStackEmpty(s) || p != NULL) {
+			while (p != NULL) {
+				push(s, p);
+				p = p->left;
+			}
+			p = pop(s);
+			if (p->right != NULL && p->right != lastVisited) {
+				push(s, p);
+				p = p->right;
+			}
+			else {
+				cout << p->data << " ";
+				lastVisited = p;
+				p = NULL;
+			}
+		}
+	}
+}
+
+struct LLQueue {
+	Node* data;
+	LLQueue* next;
+};
+
+LLQueue* createNode2(Node* data) {
+	LLQueue* p = new LLQueue;
+	p->data = data;
+	p->next = NULL;
+	return p;
+}
+
+struct Queue {
+	LLQueue* head;
+};
+
+void initQueue(Queue& q) {
+	q.head = NULL;
+}
+
+bool isQueueEmpty(Queue q) {
+	return q.head == NULL;
+}
+
+void enQueue(Queue& q, Node* data) {
+	if (isQueueEmpty(q))
+		q.head = createNode2(data);
+	else {
+		LLQueue* p = q.head;
+		while (p->next != NULL) {
+			p = p->next;
+		}
+		p->next = createNode2(data);
+	}
+}
+
+Node* deQueue(Queue& q) {
+	if (isQueueEmpty(q))
+		return NULL;
+	Node* p = q.head->data;
+	LLQueue* temp = q.head;
+	q.head = q.head->next;
+	delete temp;
+	return p;
+}
+
+void BFS(Node* root) {
+	if (root != NULL) {
+		Node* p = root;
+		Queue q;
+		initQueue(q);
+		enQueue(q, p);
+		while (!isQueueEmpty(q)) {
+			p = deQueue(q);
+			cout << p->data << " ";
+			if (p->left != NULL)
+				enQueue(q, p->left);
+			if (p->right != NULL)
+				enQueue(q, p->right);
+		}
+	}
+}
+
 int main() {
-	Node * t;
+	Node* t;
 	initBinaryTree(t);
-	insert(t, 1);
-	insert(t, 2);
-	insert(t, 3);
-	insert(t, 4);
-	insert(t, 5);
+	recursiveInsert(t, 10);
+	recursiveInsert(t, 7);
+	recursiveInsert(t, 13);
+	recursiveInsert(t, 14);
+	recursiveInsert(t, 11);
+	recursiveInsert(t, 12);
+	recursiveInsert(t, 6);
+	recursiveInsert(t, 8);
+	recursiveInsert(t, 9);
+	//cout << "So luong node: " << countNode(t) << endl;
+	//int count = 0;
+	//countNodeTailRecursion(t, count);
+	//cout << "So luong node: " << count << endl;
+	//countPrimeNodeTailRecursion(t, count);
+	//cout << "So luong node nguyen to: " << count << endl;
+	//cout << "So luong node nguyen to: " << countPrimeNode(t) << endl;
+	//BFS(t);
 	LNR(t);
+	cout << endl;
+	recursiveDel(t, 10);
+	LNR(t);
+	cout << endl;
+	//LRNNoRecursion(t);
 	//if (search(t, 1)) cout << "Tim thay!\n";
 	system("pause");
 	return 0;
---
+++ BTGKCTDL/BTGKCTDL/BT4_AVL.cpp
@@ -0,0 +1,433 @@
+#include <iostream>
+#include <Windows.h>
+#define MAX 100
+#define MAXQ 100
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left, * right;
+	int height;
+};
+
+Node* createNode(int x) {
+	Node* p = new Node;
+	p->data = x;
+	p->left = p->right = NULL;
+	p->height = 1;
+	return p;
+}
+
+struct AVLTree {
+	Node* root;
+};
+
+void initAVLTree(AVLTree& t) {
+	t.root = NULL;
+}
+
+int getHeight(Node* node) {
+	if (node != NULL)
+		return node->height;
+	return 0;
+}
+
+void updateHeight(Node*& root) {
+	if (root == NULL)
+		return;
+	updateHeight(root->left);
+	updateHeight(root->right);
+	root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+}
+
+int checkBalance(Node* root) {
+	if (root != NULL)
+		return getHeight(root->left) - getHeight(root->right);
+	return 0;
+}
+
+Node* rotateLeft(Node*& node) {
+	Node* p = node;
+	node = node->right;
+	p->right = node->left;
+	node->left = p;
+	return node;
+}
+
+Node* rotateRight(Node*& node) {
+	Node* p = node;
+	node = node->left;
+	p->left = node->right;
+	node->right = p;
+	return node;
+}
+
+void insert(AVLTree& t, int x) {
+	if (t.root == NULL)
+		t.root = createNode(x);
+	else {
+		Node* p = t.root, * pre = NULL;
+		while (p != NULL) {
+			pre = p;
+			if (p->data == x)
+				return;
+			else if (p->data > x)
+				p = p->left;
+			else
+				p = p->right;
+		}
+		p = createNode(x);
+		if (pre->data > x)
+			pre->left = p;
+		else
+			pre->right = p;
+	}
+	updateHeight(t.root);
+	int index = checkBalance(t.root);
+	if (index > 1) {
+		//Lech trai phai
+		if (checkBalance(t.root->left) < -1) {
+			t.root->left = rotateLeft(t.root->left);
+			t.root = rotateRight(t.root);
+		}
+		//Lech trai
+		else {
+			t.root = rotateRight(t.root);
+		}
+		updateHeight(t.root);
+	}
+	else if (index < -1) {
+		//Lech phai trai
+		if (checkBalance(t.root->right) > 1) {
+			t.root->right = rotateRight(t.root->right);
+			t.root = rotateLeft(t.root);
+		}
+		else {
+			t.root = rotateLeft(t.root);
+		}
+		updateHeight(t.root);
+	}
+}
+
+void del(AVLTree& t, int key) {
+	Node* p = t.root, * pre = NULL;
+	while (p != NULL && p->data != key) {
+		pre = p;
+		if (p->data > key)
+			p = p->left;
+		else
+			p = p->right;
+	}
+	if (p != NULL) {
+		if (p->left == NULL) {
+			if (pre == NULL)
+				t.root = t.root->right;
+			else {
+				if (pre->data > key)
+					pre->left = p->right;
+				else
+					pre->right = p->right;
+			}
+			delete p;
+		}
+		else if (p->right == NULL) {
+			if (pre == NULL)
+				t.root = t.root->left;
+			else {
+				if (pre->data > key)
+					pre->left = p->left;
+				else
+					pre->right = p->left;
+			}
+			delete p;
+		}
+		else {
+			Node* q = p->right, * pre = NULL;
+			while (q->left != NULL) {
+				pre = q;
+				q = q->left;
+			}
+			p->data = q->data;
+			if (pre != NULL)
+				pre->left = q->right;
+			else
+				p->right = q->right;
+			delete q;
+		}
+	}
+	updateHeight(t.root);
+	int index = checkBalance(t.root);
+	if (index > 1) {
+		//Lech trai phai
+		if (checkBalance(t.root->left) < -1) {
+			t.root->left = rotateLeft(t.root->left);
+			t.root = rotateRight(t.root);
+		}
+		//Lech trai
+		else {
+			t.root = rotateRight(t.root);
+		}
+		updateHeight(t.root);
+	}
+	else if (index < -1) {
+		//Lech phai trai
+		if (checkBalance(t.root->right) > 1) {
+			t.root->right = rotateRight(t.root->right);
+			t.root = rotateLeft(t.root);
+		}
+		else {
+			t.root = rotateLeft(t.root);
+		}
+		updateHeight(t.root);
+	}
+}
+
+void LNR(Node* root) {
+	if (root == NULL)
+		return;
+	LNR(root->left);
+	cout << root->data << "(" << root->height << ") | ";
+	LNR(root->right);
+}
+
+void NLR(Node* root) {
+	if (root == NULL)
+		return;
+	cout << root->data << "(" << root->height << ") | ";
+	NLR(root->left);
+	NLR(root->right);
+}
+
+void LRN(Node* root) {
+	if (root == NULL)
+		return;
+	LRN(root->left);
+	LRN(root->right);
+	cout << root->data << "(" << root->height << ") | ";
+}
+
+struct Stack {
+	Node* a[MAX];
+	int top;
+};
+void initS(Stack& s) {
+	s.top = -1;
+}
+
+bool isSEmpty(Stack s) {
+	return s.top == -1;
+}
+
+void push(Stack& s, Node* node) {
+	if (s.top < MAX)
+		s.a[++s.top] = node;
+}
+
+Node* pop(Stack& s) {
+	if (!isSEmpty(s))
+		return s.a[s.top--];
+}
+
+void nonRecursiveLNR(AVLTree t) {
+	Stack s;
+	initS(s);
+	Node* p = t.root;
+	while (!isSEmpty(s) || p != NULL) {
+		while (p != NULL) {
+			push(s, p);
+			p = p->left;
+		}
+		p = pop(s);
+		cout << p->data << "(" << p->height << ") | ";
+		p = p->right;
+	}
+}
+
+void nonRecursiveNLR(AVLTree t) {
+	Stack s;
+	initS(s);
+	Node* p = t.root;
+	while (!isSEmpty(s) || p != NULL) {
+		while (p != NULL) {
+			cout << p->data << "(" << p->height << ") | ";
+			push(s, p);
+			p = p->left;
+		}
+		p = pop(s);
+		p = p->right;
+	}
+}
+
+void nonRecursiveLRN(AVLTree t) {
+	Stack s;
+	initS(s);
+	Node* p = t.root, * lastVisited = NULL;
+	while (!isSEmpty(s) || p != NULL) {
+		while (p != NULL) {
+			push(s, p);
+			p = p->left;
+		}
+		p = pop(s);
+		if (p->right != NULL && lastVisited != p->right) {
+			push(s, p);
+			p = p->right;
+		}
+		else {
+			cout << p->data << "(" << p->height << ") | ";
+			lastVisited = p;
+			p = NULL;
+		}
+	}
+}
+
+struct LLQueue {
+	Node* data;
+	LLQueue* next;
+};
+
+LLQueue* createNode(Node* node) {
+	LLQueue* p = new LLQueue;
+	p->data = node;
+	p->next = NULL;
+	return p;
+}
+
+void initLLQueue(LLQueue*& q) {
+	q = NULL;
+}
+
+bool isLLQEmpty(LLQueue* q) {
+	return q == NULL;
+}
+
+void enLLQueue(LLQueue*& q, Node* node) {
+	if (q == NULL)
+		q = createNode(node);
+	else {
+		LLQueue* p = q;
+		while (p->next != NULL)
+			p = p->next;
+		p->next = createNode(node);
+	}
+}
+
+Node* deLLQueue(LLQueue*& q) {
+	if (q != NULL) {
+		Node* p = q->data;
+		q = q->next;
+		return p;
+	}
+	return NULL;
+}
+
+void BFSWLKQueue(AVLTree& t) {
+	Node* p = t.root;
+	LLQueue* q;
+	initLLQueue(q);
+	enLLQueue(q, p);
+	while (!isLLQEmpty(q)) {
+		p = deLLQueue(q);
+		cout << p->data << "(" << p->height << ") | ";
+		if (p->left != NULL)
+			enLLQueue(q, p->left);
+		if (p->right != NULL)
+			enLLQueue(q, p->right);
+	}
+}
+//Phuong phap tinh tien
+//struct Queue {
+//	Node* a[MAX];
+//	int front, rear;
+//};
+//
+//bool isQEmpty(Queue q) {
+//	return q.front == -1;
+//}
+//
+//bool isQFull(Queue q) {
+//	return q.rear - q.front == MAXQ;
+//}
+//
+//void initQueue(Queue& q) {
+//	q.front = q.rear = -1;
+//}
+//
+//void enQueue(Queue& q, Node* node) {
+//	if (isQFull(q))
+//		return;
+//	if (!isQEmpty(q)) {
+//		q.front = q.rear = 0;
+//		q.a[q.front] = node;
+//	}
+//	else {
+//		if (q.rear == MAX - 1) {
+//			for (int i = q.front; i <= q.rear; i++) {
+//				q.a[i - q.front] = q.a[i];
+//			}
+//			q.rear -= q.front;
+//			q.front = 0;
+//		}
+//		q.a[++q.rear] = node;
+//	}
+//}
+//
+//
+//Node* deQueue(Queue& q) {
+//	if (!isQEmpty(q) && q.front <= q.rear)
+//		return q.a[q.front++];
+//	return NULL;
+//}
+
+//Phuong phap vong
+struct Queue {
+	Node* a[MAXQ];
+	int front, rear, size;
+};
+
+void initQueue(Queue& q) {
+	q.front = q.rear = -1;
+	q.size = 0;
+}
+
+bool isQFull(Queue q) {
+	return q.size == MAXQ;
+}
+
+bool isQEmpty(Queue q) {
+	return q.front == -1;
+}
+
+void enQueue(Queue& q, Node* node) {
+	if (isQFull(q))
+		return;
+	if (!isQEmpty(q))
+		q.front = 0;
+	q.rear = (q.rear + 1) % MAXQ;
+	q.a[q.rear] = node;
+	q.size++;
+}
+
+Node* deQueue(Queue& q) {
+	if (isQEmpty(q))
+		return NULL;
+	Node* p = q.a[q.front];
+	q.front = (q.front + 1) % MAX;
+	q.size--;
+	if (q.size == 0)
+		q.front = q.rear = -1;
+}
+
+int main() {
+	AVLTree t;
+	initAVLTree(t);
+	insert(t, 10);
+	insert(t, 15);
+	insert(t, 5);
+	insert(t, 20);
+	insert(t, 3);
+	NLR(t.root);
+	cout << endl;
+	BFSWLKQueue(t);
+	system("pause");
+	return 0;
+}
